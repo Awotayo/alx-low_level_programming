6ff1ab557d106301715afb4f05999aa900dd7f45
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-10 19:05:56 +0000

    Hi

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers of base 10 starting from
+ * Using only putchar function
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
