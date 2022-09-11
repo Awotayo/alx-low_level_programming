5211c83aaf11b8c32a13f60e2c95d4443ddc9883
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-11 00:40:11 +0000

    Hi

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
