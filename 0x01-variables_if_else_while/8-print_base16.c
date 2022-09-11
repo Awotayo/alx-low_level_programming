df6a88aef85aedb49d4201bf48f0d2a228e17a2f
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-11 01:12:23 +0000

    Hi

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,8 +10,11 @@ int main(void)
 
 	for (n = 0; n <= 9; n++)
 		putchar((n % 10) + '0');
+
 	for (ch = 'a'; ch <= 'f'; ch++)
-	        putchar(ch);
+		putchar(ch);
+
 	putchar('\n');
+
 	return (0);
 }
