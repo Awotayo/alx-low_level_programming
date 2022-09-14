151345889a10831e193ef7186caa4ff9bb7a47d8
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-14 22:40:15 +0000

    Hi

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: value to compute
+ * Return: absolute value of an integer
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
