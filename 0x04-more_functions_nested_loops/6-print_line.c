ab648e40d4d662478bff7ecb4d5d2a49498c8671
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-16 01:44:49 +0000

    Hi

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ * @n: number of count
+ */
+void print_line(int n)
+{
+	int k;
+
+	if (n < 0)
+	{
+		for (k = 0; k < n; k++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
