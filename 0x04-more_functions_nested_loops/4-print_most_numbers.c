ffb8489afd87bf83d09165031bc5edbfd15f358c
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-16 01:16:26 +0000

    Hi

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,11 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = 0; n <= 9; n++)
-		if (n == 2 && n == 4)
-		{
-			break;
-		}
-		else
+	{
+		if (n != 2 && n != 4)
 			_putchar((n % 10) + '0');
+	}
 
-		_putchar ('\n');
+	_putchar ('\n');
 
 }
