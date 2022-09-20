7967757cee65e7db536810f01520de6affb570c0
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-20 21:44:35 +0000

    Hi

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to reverse
+ */
+
+void rev_string(char *s)
+{
+	int i, j, k;
+	char tmp;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	j = i - 1;
+	for (k = 0; k < i / 2; i++)
+	{
+		tmp = s[k];
+		s[k] = s[j];
+		s[j--] = tmp;
+	}
+}
+
