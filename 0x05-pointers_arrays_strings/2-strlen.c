dd3680801186172f15c228f8804d1e7aabaa1740
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-20 02:37:17 +0000

    Hi

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: string to be checked
+ * Return: length of *s
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	for (len = 0; len != '\0'; len++)
+		return (len);
+}
