0e044ce5241b943112d4f23b404ae1e9628cb451
Awotayo <mabolaje.jamiu@gmail.com>
2022-09-22 00:43:55 +0000

    Hi

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * *string_toupper - hanges all lowercase letters of a string to uppercase.
+ * @s: The string to be changed.
+ * Return: A pointer to the changed string.
+ */
+
+char *string_toupper(char *s)
+{
+	int index = 0;
+
+	while (s[index])
+	{
+		if (s[index] >= 'a' && s[index] <= 'z')
+			s[index] -= 32;
+		index++;
+	}
+	return (s);
+}
