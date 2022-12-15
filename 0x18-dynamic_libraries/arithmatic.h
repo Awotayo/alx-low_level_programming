7c68a673ef3c0c66e0ac89d7b7d93db56a84b8a6
Awotayo <mabolaje.jamiu@gmail.com>
2022-12-15 21:39:10 +0000

    huh

---
+++ 0x18-dynamic_libraries/arithmatic.h
@@ -0,0 +1,13 @@
+#ifndef ARITHMATIC_H
+#define ARITHMATIC_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int addNum(int a, int b);
+int subNum(int a, int b);
+int mulNum(int a, int b);
+int divNum(int a, int b);
+int mdNum(int a, int b);
+
+#endif
---
+++ 0x18-dynamic_libraries/function.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "arithmatic.h"
+
+int addNum(int a, int b)
+{
+	int nAdd = a + b;
+	return nAdd;
+}
+int subNum(int a, int b)
+{
+	int nsub = a - b;
+	return nsub;
+}
+int mulNum(int a, int b)
+{
+	int nmul = a * b;
+	return nmul;
+}
+int divNum(int a, int b)
+{
+	int ndiv = a / b;
+	return ndiv;
+}
+int mdNum(int a, int b)
+{
+	int nmd = a % b;
+	return nmd;
+}
